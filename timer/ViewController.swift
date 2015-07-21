a96e12e2ca3e379cf7ff21e1f4eba5f1aa3f1fc8
松浦 大 <matsuura_dai@cyberagent.co.jp>
2015-07-21 22:18:04 +0900

    Fixed toggle function and a few bug

--- timer/ViewController.swift
+++ timer/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var toggleButton: UIButton!
     @IBOutlet weak var timerPicker: UIDatePicker!
+    @IBOutlet weak var resetButton: UIButton!
     var timer: NSTimer = NSTimer()
 
     var settingTime: String = ""
@@ -22,21 +23,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        // 現在設定されている時間を秒単位で取得
-        //currentTotalSeconds = getCurrentSeconds(timeLabel.text!)
-        //initialTotalSeconds = currentTotalSeconds
-        
+        initSetTimer()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
     
     override func viewWillAppear(animated: Bool){
         // ラベルを非表示
-        // timeLabel.hidden = true
+        self.timeLabel.hidden = true
     }
     
     @IBAction func timerPickerAction(sender: AnyObject) {
@@ -47,28 +45,57 @@ class ViewController: UIViewController {
         
     }
     
+    // Start, Stopボタン
     @IBAction func toggleTimer(sender: UIButton) {
-        // self.timerPicker.hidden = true
-        // self.timeLabel.hidden = false
+        
         settingTime = self.timeLabel.text!
-        var timeArray = split(settingTime, { $0 == ":" })
-        var hours = timeArray[0].toInt()!
-        var minutes = timeArray[1].toInt()!
-        var seconds = timeArray[2].toInt()!
-        var totalSeconds:Int = hours * 60 * 60 + minutes * 60 + seconds
+        
+        switch sender.tag {
+            case 0:
+                currentTotalSeconds = changeTimeInSecondsFormat(settingTime, isSeconds: false)
+                initialTotalSeconds = currentTotalSeconds
+            case 1:
+                currentTotalSeconds = changeTimeInSecondsFormat(settingTime, isSeconds: true)
+                
+            default:
+            
+            break
+        }
+        
+        sender.tag = 1
+        
         
         if timer.valid {
             stopTimer()
+            
         } else {
+            // self.resetButton.enabled = false;
+            self.timeLabel.text = changeRemainingTimeFormat(currentTotalSeconds);
+            self.timerPicker.hidden = true
+            self.timeLabel.hidden = false
+            
+
             timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "onUpdate:", userInfo: nil, repeats: true)
             sender.setTitle("Stop", forState: UIControlState.Normal)
         }
         
     }
     
+    func initSetTimer() {
+        let initialSettingTime = "00:03:00"
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss"
+        var date = NSDate()
+        date = NSDate(timeInterval: 0, sinceDate: dateFormatter.dateFromString(initialSettingTime)!)
+        
+        timerPicker.date = date;
+        self.timeLabel.text = initialSettingTime
+
+    }
+    
     func initLabelText(){
         currentTotalSeconds = initialTotalSeconds
-        timeLabel.text = changeRemainingTimeFormat(initialTotalSeconds)
+        self.timeLabel.text = changeRemainingTimeFormat(initialTotalSeconds)
     }
     
     func stopTimer(){
@@ -76,11 +103,12 @@ class ViewController: UIViewController {
         toggleButton.setTitle("Start", forState: UIControlState.Normal)
     }
     
-    func getCurrentSeconds(currentTime: String) -> Int {
+    func changeTimeInSecondsFormat(currentTime: String, isSeconds: Bool) -> Int {
+        
         var currentTimeArray = split(currentTime, { $0 == ":"})
         var currentHours = currentTimeArray[0].toInt()!
         var currentMinutes = currentTimeArray[1].toInt()!
-        var currentSeconds = currentTimeArray[2].toInt()!
+        var currentSeconds = isSeconds ? currentTimeArray[2].toInt()! : 0
         
         return currentHours * 60 * 60 + currentMinutes * 60 + currentSeconds
     }
@@ -94,6 +122,8 @@ class ViewController: UIViewController {
             toggleButton.setTitle("Start", forState: UIControlState.Normal)
             initLabelText()
             timer.invalidate()
+            self.timerPicker.hidden = false
+            self.timeLabel.hidden = true
             
             var alert = UIAlertView()
             alert.title = "タイマー終了"
@@ -104,8 +134,8 @@ class ViewController: UIViewController {
     
     func changeRemainingTimeFormat(seconds: Int) -> String {
         var destHours = seconds / (60 * 60);
-        var destMinutes = seconds / 60;
-        var destSeconds = seconds % 60;
+        var destMinutes = (seconds % (60 * 60)) / 60;
+        var destSeconds = (seconds % (60 * 60)) % 60;
         var desthoursStr: String = String(destHours)
         var destMinutesStr: String = (countElements(destMinutes.description) == 1) ? "0" + destMinutes.description : destMinutes.description
         var destSecondsStr: String = (countElements(destSeconds.description) == 1) ? "0" + destSeconds.description : destSeconds.description
@@ -117,7 +147,14 @@ class ViewController: UIViewController {
     @IBAction func resetTimer(sender: UIButton) {
         stopTimer()
         initLabelText()
+        self.timerPicker.hidden = false
+        self.timeLabel.hidden = true
+        toggleButton.tag = 0
+        // sender.enabled = true;
     }
+    
+    // Value Change後にリセットボタン押下でStartタップすると値が反映されない
+    // Resetボタンを押せないようにする
     
 }
 
